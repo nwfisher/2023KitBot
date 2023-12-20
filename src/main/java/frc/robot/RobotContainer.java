baf7270db9384a906d8b14e571fdb22fd3a7e4e5
Nicolas Fisher <nicofisher2007@gmail.com>
2023-12-19 18:15:55 -0800

    Set TeleopDrive as default command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,14 @@
 
 package frc.robot;
 
+
+import frc.robot.commands.TeleopDrive;
+import frc.robot.subsystems.*;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
@@ -17,15 +21,20 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
+
+  private final int translationAxis = XboxController.Axis.kLeftY.value;
+  private final int rotationAxis = XboxController.Axis.kRightX.value;
+
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Drivetrain mDrivetrain = new Drivetrain();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final XboxController mDriverController = new XboxController(0);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    mDrivetrain.setDefaultCommand(new TeleopDrive(mDrivetrain, mDriverController.getRawAxis(translationAxis), mDriverController.getRawAxis(rotationAxis)));
+    
     // Configure the trigger bindings
     configureBindings();
   }
